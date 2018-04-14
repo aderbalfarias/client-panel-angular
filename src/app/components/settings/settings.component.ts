1ea9011b193ac851278748e721bbfc0272435beb
Aderbal Farias <aderbalfarias@hotmail.com>
2018-04-14 16:42:13 -0300

    Implementing settings

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { SettingsService } from '../../services/settings.service';
+import { Settings } from '../../models/Settings';
 
 @Component({
-  selector: 'app-settings',
-  templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.css']
+    selector: 'app-settings',
+    templateUrl: './settings.component.html',
+    styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit {
+    settings: Settings;
 
-  constructor() { }
+    constructor(
+        private router: Router,
+        private flashMessage: FlashMessagesService,
+        private settingsService: SettingsService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.settings = this.settingsService.getSettings();
+    }
 
+    onSubmit() {
+        this.settingsService.changeSettings(this.settings);
+        this.flashMessage.show('Settings saved', {
+            cssClass: 'alert-success', timeout: 4000
+        });
+    }
 }
